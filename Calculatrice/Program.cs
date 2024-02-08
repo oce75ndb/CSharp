d31b48e8b0937e93de3c90fb53ca6ea4033d5ae7
Océane Bondon <oceanebondon30@gmail.com>
2024-02-08 15:31:51 +0100

    commit initial

---
+++ Calculatrice/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Bienvenue dans la calculatrice !");
+
+result = 0;
+
+while (true) {
+
+    Console.Write("Entrez un nombre : ");
+    decimal nb1 = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Entrez un opérateur (+, -, *, /) : ");
+    string op = Console.ReadLine();
+
+    Console.Write("Entrez le deuxième nombre : ");
+    decimal nb2 = Convert.ToDecimal(Console.ReadLine());
+
+    switch (op) {
+
+        case "+":
+            result =  nb1 +  nb2;
+            break;
+        case "-":
+            result =  nb1 -  nb2;
+            break;
+        case "*":
+            result =  nb1 *  nb2;
+            break;
+        case "/":
+            result =  nb1 /  nb2;
+            break;
+        default:
+            Console.WriteLine("Opérateur invalide.");
+            break;
+    }
+
+    Console.WriteLine($"Le résultat de {nb1} {op} {nb2} = {result}");
+}
+
