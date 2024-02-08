89df53aaa8e80a9cae897396270b454bba67a5ea
Océane Bondon <oceanebondon30@gmail.com>
2024-02-08 12:22:55 +0100

    commit initial

---
+++ NombreMystere/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Nombre Mystère");
+
+// Définir un nombre mystère entre 1 et 100 sous forme de constante entière
+// Demander le nom du user et afficher msg de bienvenue
+// Demander un nombre au user
+
+//constante
+
+Console.Write("Veuillez entrer votre prénom : ");
+string prenom = Console.ReadLine();
+
+Console.Write("Veuillez entrer un nombre : ")
+int nb = Console.ReadLine();
+
+Console.WriteLine($"Hello {prenom}, bon retour parmis nous ! ");
+Console.WriteLine($"Votre nombre est {nb}.");
