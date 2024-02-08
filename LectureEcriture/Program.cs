8f54d43fb097cb6f8f1d043f8a185c2ea5ee0d42
Océane Bondon <oceanebondon30@gmail.com>
2024-02-08 11:44:45 +0100

    commit initial

---
+++ LectureEcriture/Program.cs
@@ -0,0 +1,6 @@
+﻿// Demander le nom de l'utilisateur et afficher un message de bienvenue personnalisé
+
+Console.WriteLine("Veuillez entrer votre prénom : ");
+string prenom = Console.ReadLine();
+
+Console.WriteLine($"Hello {prenom}, bon retour parmis nous ! ");
