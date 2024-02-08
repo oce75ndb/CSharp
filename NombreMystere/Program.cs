099331f8dba221f1b74bb7dab65a04a95ab5c960
Océane Bondon <oceanebondon30@gmail.com>
2024-02-08 13:59:20 +0100

    commit 3

--- NombreMystere/Program.cs
+++ NombreMystere/Program.cs
@@ -4,13 +4,17 @@
 // Demander le nom du user et afficher msg de bienvenue
 // Demander un nombre au user
 
-const int Max = 100;
+const int NOMBRE_MYSTERE = 100;
+const int MIN = 0;
+const int MAX = 100;
 
 Console.Write("Veuillez entrer votre prénom : ");
 string prenom = Console.ReadLine();
 
-Console.Write("Veuillez entrer un nombre : ");
+Console.Write("Veuillez entrer un nombre entre {MIN} et {MAX}: ");
 string nb = Console.ReadLine();
+
+int.Parse(Console.WriteLine)
 
 Console.WriteLine($"Hello {prenom}, bon retour parmis nous ! ");
 Console.WriteLine($"Votre nombre est {nb}.");
