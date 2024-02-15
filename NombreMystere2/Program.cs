9169a883ff824b61193bae43ee0719c22451bc9b
Océane Bondon <oceanebondon30@gmail.com>
2024-02-15 12:22:40 +0100

    commit initial

---
+++ NombreMystere2/Program.cs
@@ -0,0 +1,34 @@
+﻿const int MAX = 100;
+const int MIN = 1;
+const int NOMBRE_MYSTERE = 42;
+
+Console.Write("Veuillez entrer votre prénom : ");
+string prenom = Console.ReadLine();
+
+bool trouve = false;
+List<int> nombreSaisis = new List<int>();
+
+while(!trouve) {
+    int nbSaisi = 0;
+
+    Console.Write("Nombres déjà proposés : ");
+    foreach (int nb in nombreSaisis)
+    {
+            Console.Write($"{nb} ");
+    }
+    Console.WriteLine(); //passage à la ligne
+    Console.WriteLine($"Veuillez entrer un nombre entre {MIN} et {MAX}");
+    nbSaisi = int.Parse(Console.ReadLine());
+    nombreSaisis.Add(nbSaisi);
+
+    if(nbSaisi == NOMBRE_MYSTERE) {
+        Console.WriteLine("Vous avez trouvé !!");
+        trouve = true;
+    }
+    else {
+        if(nbSaisi>NOMBRE_MYSTERE)
+            Console.WriteLine("Le nombre mystère est plus petit");
+        else
+            Console.WriteLine("Le nombre mystère est plus grand");
+    }
+}
