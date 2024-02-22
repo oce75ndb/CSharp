5bd4f803ceeab3c9d7e213053fb12e85570724a3
Océane Bondon <oceanebondon30@gmail.com>
2024-02-22 17:00:31 +0100

    commit init

---
+++ Fonction/Program.cs
@@ -0,0 +1,13 @@
+﻿int addition(int a, int b) {
+    return a+b;
+}
+
+Console.WriteLine(addition(1,2));
+
+int factorielle(int a) {
+    int c = 1;
+    for(int i = 2; i >= a; i++) {
+
+    }
+}
+Console.WriteLine(factorielle());
