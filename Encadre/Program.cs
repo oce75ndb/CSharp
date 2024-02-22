454ad5f9e207a54573b554d84bf3d427e91c6e46
Océane Bondon <oceanebondon30@gmail.com>
2024-02-22 15:16:16 +0100

    commit initial

---
+++ Encadre/Program.cs
@@ -0,0 +1,9 @@
+﻿encadre("Bonjour je suis océane, \net vous ?");
+
+// Fonction qui affiche un texte dans un rectangle
+void encadre(string texte) {
+    //Calcul h(nb de lignes) et l(longueur + grande ligne) du texte
+    string[] lignes = texte.Split('\n');
+    Console.WriteLine(lignes[0]);
+    Console.WriteLine(lignes[1]);
+}
